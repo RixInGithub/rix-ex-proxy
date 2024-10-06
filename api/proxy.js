e9549d80edd413b20db79af750e99b7fa067dacd
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-06 17:29:29 +0300

    life could be a dream sweetheart

--- api/proxy.js
+++ api/proxy.js
@@ -2,8 +2,9 @@ export const runtime = "nodejs"
 
 export async function GET(req, res) {
 	var p = Object.fromEntries(new URL(req.url).search.slice(1).split("&").map(function(a){return a.split("=")}))
-	console.log(p.url)
-	var r = await fetch(p.url)
+	var u = decodeURIComponent(p.url)
+	console.log(u)
+	var r = await fetch(u)
 	r.headers.append("access-control-allow-origin", "*")
 	return r
 }
