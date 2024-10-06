66a691d913d634692b118da6d1755ff60ab9cc06
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-06 17:31:03 +0300

    every time i look at you

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ export async function GET(req, res) {
 	var p = Object.fromEntries(new URL(req.url).search.slice(1).split("&").map(function(a){return a.split("=")}))
 	var u = decodeURIComponent(p.url)
 	console.log(u)
-	var r = await fetch(u)
+	var r = new Response(await fetch(u))
 	r.headers.append("access-control-allow-origin", "*")
 	return r
 }
