2283248cde3b12cd04d0f1f19a134d800912591f
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-06 17:51:14 +0300

    and tell me darling im the only one that you love

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export const runtime = "nodejs"
+export runtime = "nodejs"
 
 export async function GET(req, res) {
 	var p = Object.fromEntries(new URL(req.url).search.slice(1).split("&").map(function(a){return a.split("=")}))
