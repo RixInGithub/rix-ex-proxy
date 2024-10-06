0711d5534ff982cf9a71af47320dfdd5f28be517
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-06 17:27:57 +0300

    if you would let me spend my whole life loving you

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@ export const runtime = "nodejs"
 
 export async function GET(req, res) {
 	var p = Object.fromEntries(new URL(req.url).search.slice(1).split("&").map(function(a){return a.split("=")}))
-	console.log(p)
+	console.log(p.url)
 	var r = await fetch(p.url)
 	r.headers.append("access-control-allow-origin", "*")
 	return r
