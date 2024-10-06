6c63fb6eb0fa84fa4133506a5943eb02aa31ddd5
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-06 17:26:55 +0300

    if only all my precious plans would come true
    i agree

--- api/proxy.js
+++ api/proxy.js
@@ -1,9 +1,9 @@
 export const runtime = "nodejs"
 
 export async function GET(req, res) {
-	var p = new URL(req.url).search.slice(1).split("&").map(function(a){return a.split("=")})
+	var p = Object.fromEntries(new URL(req.url).search.slice(1).split("&").map(function(a){return a.split("=")}))
 	console.log(p)
-	var r = await fetch(Object.fromEntries(p).url)
+	var r = await fetch(p.url)
 	r.headers.append("access-control-allow-origin", "*")
 	return r
 }
