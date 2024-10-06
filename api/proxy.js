bd3c8d0870876b3d63946ba53b029e8a55314a32
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-06 17:02:08 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export const runtime = "nodejs"
 
 export async function GET(req, res) {
-	return await fetch(new URL(req.url).searchParams.url)
+	return await fetch(new URL(req.url).searchParams.get("url"))
 }
