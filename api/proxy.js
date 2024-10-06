1fa2fa1fe7c06c7d37c8522e30379f927cc29062
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-06 17:17:24 +0300

    if i could take you to a paradise up aboove

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,7 @@
 export const runtime = "nodejs"
 
 export async function GET(req, res) {
+	console.log(new URL(req.url).search)
 	var r = await fetch(Object.fromEntries(new URL(req.url).search.slice(1).split("&").map(function(a){return a.split("&")})).url)
 	r.headers.append("access-control-allow-origin", "*")
 	return r
