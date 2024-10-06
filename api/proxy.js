03448775eae72efc4ca1ead89588795601c2b1bf
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-06 17:53:54 +0300

    life could be a dream sweetheart hello hello again

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,19 @@
-export runtime = "nodejs"
-
-export async function GET(req, res) {
-	var p = Object.fromEntries(new URL(req.url).search.slice(1).split("&").map(function(a){return a.split("=")}))
-	var u = decodeURIComponent(p.url)
-	console.log(u)
-	var r = await fetch(u)
-	var h = new Headers(r.headers)
-	h.append("access-control-allow-origin", "*")
-	h.set("access-control-allow-origin", "*")
-	Object.defineProperty(r, "headers", {
-		value: h
-	})
-	Object.defineProperty(r, "body", {
-		value: await r.text()
-	})
-	return new Response(r.body, {...r})
+module.exports = {
+	async function GET(req, res) {
+		var p = Object.fromEntries(new URL(req.url).search.slice(1).split("&").map(function(a){return a.split("=")}))
+		var u = decodeURIComponent(p.url)
+		console.log(u)
+		var r = await fetch(u)
+		var h = new Headers(r.headers)
+		h.append("access-control-allow-origin", "*")
+		h.set("access-control-allow-origin", "*")
+		Object.defineProperty(r, "headers", {
+			value: h
+		})
+		Object.defineProperty(r, "body", {
+			value: await r.text()
+		})
+		return new Response(r.body, {...r})
+	},
+	runtime: "nodejs"
 }
